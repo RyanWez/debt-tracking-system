a38acd8a65186625b1ff0bad018e9483bab5bd95
Ryan Wez <ryanwez25@gmail.com>
2025-11-21 14:11:01 +0630

    Version 1.5.6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Debt Tracking System PWA
-const CACHE_NAME = 'debt-tracker-v1.5.5';
+const CACHE_NAME = 'debt-tracker-v1.5.6';
 const urlsToCache = [
   './',
   './index.html',
